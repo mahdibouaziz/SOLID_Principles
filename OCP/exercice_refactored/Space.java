3dcbee5e43f496ad322a723f1471265af1eb223b
mahdibouaziz <mahdibouaziz@hotmail.fr>
2022-10-04 23:35:47 +0100

    finished the part1 of the TP

---
+++ OCP/exercice_refactored/Space.java
@@ -0,0 +1,20 @@
+package com.directi.training.ocp.exercise;
+
+public class Space implements Slot{
+    
+
+    private void markSlotFree(int resourceId)
+    {
+    }
+
+     private void markSlotBusy(int resourceId)
+    {
+    }
+
+     private int findFreeSlot()
+    {
+        return 0;
+    }
+
+   
+}
---
+++ OCP/exercice_refactored/Time.java
@@ -0,0 +1,18 @@
+package com.directi.training.ocp.exercise;
+
+public class Time implements Slot{
+
+    private void markSlotFree(int resourceId){
+
+    } 
+
+     private void markSlotBusy(int resourceId)
+    {
+    }
+
+     private int findFreeSlot()
+    {
+        return 0;
+    }
+   
+}
